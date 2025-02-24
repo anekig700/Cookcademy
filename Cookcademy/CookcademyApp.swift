d8be245bfc2b68a3e86eda85ae6078bed8c5c58c
Ekaterina <anekig@icloud.com>
2025-02-24 15:26:26 +0000

    init

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Kotya on 22/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
